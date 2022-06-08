5e15a6ea45c17aa406a80500b1ad384342b21a15
Lucien Cartier-Tilet <lucien@phundrak.com>
2022-06-09 00:42:58 +0200

    initial commit

---
+++ test-project.el
@@ -0,0 +1,37 @@
+;;; test-project.el --- A test project with Eask  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Lucien Cartier-Tilet
+
+;; Author: Lucien Cartier-Tilet <lucien@phundrak.com>
+;; Maintainer: Lucien Cartier-Tilet <lucien@phundrak.com>
+;; URL: https://github.com/user/repo
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1") (s "1") (dash "2"))
+;; Keywords:
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; A test project with Eask
+;;
+
+;;; Code:
+
+;; Happy coding! ;)
+
+(provide 'test-project)
+;;; test-project.el ends here
